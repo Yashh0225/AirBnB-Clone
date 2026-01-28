7f7d0aa356f9de79d919ef256c19600b6c1d5cf7
Yash Sharma <yashsharma0225@gmail.com>
2026-01-29 00:19:54 +0530

    origin

--- api/index.js
+++ api/index.js
@@ -1,21 +1,57 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User.js')
+const bcrypt = require('bcryptjs');
+
+require('dotenv').config();
+//ciqrChYC66j3Oebr
 
 app.use(express.json());
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(cors({
     credentials:true,
     origin:'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get('/test',(req,res)=>{
     res.json('test okk');
 })
 
-app.post('/register',(req,res)=>{
+app.post('/register', async (req,res)=>{
     const {name,email,password} = req.body;
-    res.json({name,email,password});
+    try{
+        const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password,10),
+    })
+
+    res.json(userDoc);
+    alert('Registration successful!');
+    }
+    catch(e){
+        alert('Registration failed. Please try again later.');
+    }
+})
+
+app.post('/login', async (req,res)=>{
+    const {email,password} = req.body;
+
+    const userDoc = await User.findOne({email});
+    if(userDoc){
+        const passOk = bcrypt.compareSync(password,userDoc.password);
+        if(passOk){res.json('pass ok')}
+        else res.status(422).json('pass not ok');
+    }
+    else{
+        res.json("not found");
+    }
 })
 
 app.listen(4000,()=>{
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,19 @@ export default function LoginPage(){
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
 
-    function registerUser(ev){
+    async function registerUser(ev){
         ev.preventDefault();
-        axios.post('/register',{
+        try{
+            await axios.post('/register',{
             name,
             email,
             password
         })
+        alert('Registration successful!');
+        }catch(e){
+            alert('Registration failed. Please try again later.');
+        }
+        
     }
     return (
         <div className="mt-4 grow flex flex-col justify-around">
